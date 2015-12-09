5d8c1cd4c2c70843fbf2ecdf5382f7bf8ae64c5d
Ethan Koenig <ethantkoenig@gmail.com>
2015-12-09 08:16:34 -0500

    Documentation for alias script

--- translateAliases.sh
+++ translateAliases.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+# Creates aliases for running the system
+
 alias eng2ita='_eng2ita(){ echo $1 | '`pwd`'/Haskell/translate '`pwd`'/Haskell/Trans.pgf Eng Ita; }; _eng2ita'
 alias ita2eng='_ita2eng(){ echo $1 | '`pwd`'/Haskell/translate '`pwd`'/Haskell/Trans.pgf Ita Eng; }; _ita2eng'
 
